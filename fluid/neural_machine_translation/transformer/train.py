712844aac99c4e41d10ee8ad4f3d20b9c36783f0
gongweibao <weibao.gong@gmail.com>
2018-08-28 03:21:37 +0000

    add nccl2

--- fluid/neural_machine_translation/transformer/train.py
+++ fluid/neural_machine_translation/transformer/train.py
@@ -15,6 +15,7 @@ from optim import LearningRateScheduler
 
 import logging
 import sys
+import copy
 
 def parse_args():
     parser = argparse.ArgumentParser("Training for Transformer.")
@@ -97,6 +98,11 @@ def parse_args():
         default=True,
         help='Whether to run as local mode.')
     parser.add_argument(
+        '--update_method',
+        choices=("pserver", "nccl2"),
+        default="pserver",
+        help='Update method.')
+    parser.add_argument(
         '--device',
         type=str,
         default='GPU',
@@ -125,6 +131,64 @@ def parse_args():
                         [TrainTaskConfig, ModelHyperParams])
     return args
 
+def append_nccl2_prepare(trainer_id, worker_endpoints, current_endpoint):
+    assert(trainer_id >= 0 and 
+           len(worker_endpoints) > 1 and 
+           current_endpoint in worker_endpoints)
+
+    eps = copy.deepcopy(worker_endpoints)
+    eps.remove(current_endpoint)
+
+    nccl_id_var = fluid.default_startup_program().global_block().create_var(
+        name="NCCLID",
+        persistable=True,
+        type=fluid.core.VarDesc.VarType.RAW)
+
+    fluid.default_startup_program().global_block().append_op(
+        type="gen_nccl_id",
+        inputs={},
+        outputs={"NCCLID": nccl_id_var},
+        attrs={
+            "endpoint": current_endpoint,
+            "endpoint_list": eps,
+            "trainer_id": trainer_id
+        })
+    return nccl_id_var
+
+    """
+    if trainer_id >= 0:
+        # append gen_nccl_id at the end of startup program
+        trainer_id = int(os.getenv("PADDLE_TRAINER_ID"))
+        port = os.getenv("PADDLE_PSERVER_PORT")
+        worker_ips = os.getenv("PADDLE_TRAINER_IPS")
+        worker_endpoints = []
+        for ip in worker_ips.split(","):
+            worker_endpoints.append(':'.join([ip, port]))
+        num_trainers = len(worker_endpoints)
+        current_endpoint = os.getenv("PADDLE_CURRENT_IP") + ":" + port
+        num_trainers = len(worker_endpoints)
+        eps = copy.deepcopy(worker_endpoints)
+        eps.remove(current_endpoint)
+
+        nccl_id_var = fluid.default_startup_program().global_block().create_var(
+            name="NCCLID",
+            persistable=True,
+            type=fluid.core.VarDesc.VarType.RAW)
+        fluid.default_startup_program().global_block().append_op(
+            type="gen_nccl_id",
+            inputs={},
+            outputs={"NCCLID": nccl_id_var},
+            attrs={
+                "endpoint": current_endpoint,
+                "endpoint_list": worker_endpoints,
+                "trainer_id": trainer_id
+            })
+        #return nccl_id_var, num_trainers, trainer_id
+        return nccl_id_var
+    else:
+        raise Exception("must set positive PADDLE_TRAINER_ID env variables for "
+                        "nccl-based dist train.")
+    """
 
 def pad_batch_data(insts,
                    pad_idx,
@@ -339,7 +403,7 @@ def test_context(train_progm, avg_cost, train_exe, dev_count, data_input_names,
 
 
 def train_loop(exe, train_progm, dev_count, sum_cost, avg_cost, lr_scheduler,
-               token_num, predict):
+               token_num, predict, nccl2_num_trainers=1, nccl2_trainer_id=0):
     # Initialize the parameters.
     if TrainTaskConfig.ckpt_path:
         fluid.io.load_persistables(exe, TrainTaskConfig.ckpt_path)
@@ -381,7 +445,7 @@ def train_loop(exe, train_progm, dev_count, sum_cost, avg_cost, lr_scheduler,
         use_cuda=TrainTaskConfig.use_gpu,
         loss_name=sum_cost.name,
         main_program=train_progm,
-        build_strategy=build_strategy)
+        build_strategy=build_strategy,num_trainers=nccl2_num_trainers, trainer_id=nccl2_trainer_id)
 
     data_input_names = encoder_data_input_fields + decoder_data_input_fields[:
                                                                              -1] + label_data_input_fields
@@ -562,9 +626,15 @@ def train(args):
         trainers = int(os.getenv("PADDLE_TRAINERS_NUM", "0"))
         current_endpoint = os.getenv("POD_IP") + ":" + port
         trainer_id = int(os.getenv("PADDLE_TRAINER_ID"))
+
+        if args.update_method == "nccl2":
+            append_nccl2_prepare(trainer_id, eplist, current_endpoint)
+            train_loop(exe, fluid.default_main_program(), dev_count, sum_cost, avg_cost,
+                       lr_scheduler, token_num, predict, trainers, trainer_id)
+            return
+
         t = fluid.DistributeTranspiler()
         t.transpile(trainer_id, pservers=pserver_endpoints, trainers=trainers)
-
         if training_role == "PSERVER":
             current_endpoint = os.getenv("POD_IP") + ":" + os.getenv(
                 "PADDLE_PORT")
@@ -576,17 +646,10 @@ def train(args):
                                                     pserver_prog)
 
             logging.info("psserver begin run")
-            #with open('pserver_startup.desc', 'w') as f:
-            #    f.write(str(pserver_startup))
-            #with open('pserver_prog.desc', 'w') as f:
-            #    f.write(str(pserver_prog))
             exe.run(pserver_startup)
             exe.run(pserver_prog)
         elif training_role == "TRAINER":
-
             trainer_prog = t.get_trainer_program()
-            with open('trainer_prog.desc', 'w') as f:
-                f.write(str(trainer_prog))
             train_loop(exe, trainer_prog, dev_count, sum_cost, avg_cost,
                        lr_scheduler, token_num, predict)
         else:
